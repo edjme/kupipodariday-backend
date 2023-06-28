6140dfb4be397ce3c07ca37e071fcd0425935a0f
Stanislav <stasikparmut@yandex.ru>
2023-06-28 15:24:14 +0300

    second try

--- src/base/base-class.ts
+++ src/base/base-class.ts
@@ -4,13 +4,20 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { IsNotEmpty, IsDate } from 'class-validator';
 
 @Entity()
 export abstract class BaseClass {
   @PrimaryGeneratedColumn()
   id: number;
+
   @CreateDateColumn()
+  @IsNotEmpty()
+  @IsDate()
   createdAt: Date;
+
   @UpdateDateColumn()
+  @IsNotEmpty()
+  @IsDate()
   updatedAt: Date;
 }
